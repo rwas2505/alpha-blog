ae161ef26f69b96896891faf9b865403989853f4
Ryan <ryanwaszak@gmail.com>
2020-07-24 18:55:20 -0500

    add new and create actions and new form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,27 @@
 class ArticlesController < ApplicationController
   def show
-    byebug
+    # byebug
     @article = Article.find(params[:id])
   end
 
   def index
     @articles = Article.all
   end
+
+  def new
+    
+  end
+
+  def create
+    # render plain: params[:article]
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    # render plain: @article.inspect
+    @article.save
+    
+    # redirect_to article_path(@article) //the below code is shorthand for this line
+    redirect_to @article
+
+    #display errors like validation errors
+  end
+  
 end
